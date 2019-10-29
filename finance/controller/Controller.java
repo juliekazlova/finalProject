41c1312836311705f556da97e4de8b5c0167f80b
juliekazlova <55464729+juliekazlova@users.noreply.github.com>
2019-10-30 01:08:33 +0300

    Add files via upload

--- finance/controller/Controller.java
+++ finance/controller/Controller.java
@@ -1,19 +1,49 @@
 package by.epam.java.kazlova.finance.controller;
 
+import by.epam.java.kazlova.finance.services.*;
+
 public class Controller {
     private final char paramDelimeter = ' ';
 
     public String executeTask(String request){
         String command;
         command = request.substring(0, request.indexOf(paramDelimeter));
-        command = command.toUpperCase();
+        String[] data=command.split(" ");
         String response = null;
-        switch(command){
+        switch(data[0]){
             case "SIGN_IN":
 // do action (call services and others) and create a response
+                SignInCommand signInCommand=new SignInCommand();
+                try {
+                    signInCommand.execute(command);
+                } catch (ServiceException e){
+                    response="SignIn error!";
+                }
                 break;
             case "ADD_ACCOUNT":
 // do action and create a response
+                AddAccountCommand addAccountCommand=new AddAccountCommand();
+                try {
+                    addAccountCommand.execute(command);
+                } catch (ServiceException e){
+                    response="Add account error!";
+                }
+                break;
+            case "BLOCK_ACCOUNT":
+                BlockAccountCommand blockAccountCommand=new BlockAccountCommand();
+                try {
+                    blockAccountCommand.execute(command);
+                } catch (ServiceException e){
+                    response="Account blocking error!";
+                }
+                break;
+            case "TRANSACTION":
+                AccountTransactionCommand accountTransactionCommand=new AccountTransactionCommand();
+                try {
+                    accountTransactionCommand.execute(command);
+                } catch (ServiceException e){
+                    response="Transaction error!";
+                }
                 break;
             default:
                 response = "We can't execute this command";
